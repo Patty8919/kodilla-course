e208d6625a4fe3d0c5a8b431c84c93145bb8ca50
Patty8919 <pat.maciejewska@gmail.com>
2019-12-14 22:27:19 +0100

    32.3

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,11 +1,15 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.trello.client.TrelloClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MailCreatorService {
@@ -17,7 +21,16 @@ public class MailCreatorService {
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
+    @Autowired
+    private TrelloClient trelloClient;
+
     public String buildTrelloCardEmail (String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/crud");
@@ -26,6 +39,23 @@ public class MailCreatorService {
         context.setVariable("goodbye_message", "Dziękuje, miłego dnia!");
         context.setVariable("companyName", adminConfig.getCompanyName());
         context.setVariable("preview_message", "Trello board update info");
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
+        return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    public String buildSecondMail (String message) {
+        Context context = new Context();
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye_message", "Dziękuje, miłego dnia!");
+        context.setVariable("companyName", adminConfig.getCompanyName());
+        context.setVariable("preview_message", "Trello board update info");
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("daily_update", trelloClient.getTrelloBoards().size());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
